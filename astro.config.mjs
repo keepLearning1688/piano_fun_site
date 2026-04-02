d45d407e41a3342b9a679403ec012e20308b54bf
Michael Liu <keepLearning1688@gmail.com>
2026-04-01 21:40:16 -0700

    routing update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    // This is the specific Astro 6 fix.
-    // It tells Astro: "Don't make a fancy config, just make a Worker file."
-    mode: 'advanced',
     imageService: 'passthrough'
   }),
   vite: {
